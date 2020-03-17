9b768caea854f7db88d685de92bb76969b77fc6d
Ashley Huang <ashleyhuang0201@gmail.com>
2020-03-17 20:38:43 +1100

    background

--- design.py
+++ design.py
@@ -1,1 +1,23 @@
 import arcade
+
+def white_circle(x, y):
+    arcade.draw_circle_filled(x,y, 40, arcade.color.WHITE, 40)
+
+
+def main():
+
+    SCREEN_WIDTH = 1600
+    SCREEN_HEIGHT = 900
+    arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Game")
+
+    arcade.set_background_color(arcade.color.BLACK)
+
+    arcade.start_render()
+    white_circle(500,500)
+
+    arcade.finish_render()
+
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
